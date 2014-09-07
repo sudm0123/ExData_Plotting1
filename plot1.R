739e7cd48276f55758ec24d296bfdfac3f635781
Sudhanshu <sudm0123@yahoo.com>
2014-09-08 04:29:43 +0530

    Course Project 1 File : Final

--- plot1.R
+++ plot1.R
@@ -1,8 +1,14 @@
-data<-read.table("household_power_consumption.txt",sep=";",head=TRUE)   ## Code for Reading the Data[Start]
+## Code for Reading the Data[Start]
+data<-read.table("household_power_consumption.txt",sep=";",head=TRUE)
 x<-data[,1]
 x<-strptime(x,"%d/%m/%Y")
 y<-(x=="2007-02-01 IST" | x=="2007-02-02 IST")
-z<-data[Y,]
-data<-z   ## Code for Reading the Data[End]
+z<-data[y,]
+data<-z   
+## Code for Reading the Data[End]
+
 ## Code for Histogram
-hist(as.numeric(as.character(data$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+hist(as.numeric(as.character(data$Global_active_power)),col="red",
+     main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.copy(png,file = "plot1.png")
+dev.off()
